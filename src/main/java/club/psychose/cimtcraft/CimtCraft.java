febdbc9c20c7c432dc8217e4a5ff127d649e9297
CrashedLife <33805688+CrashedLife@users.noreply.github.com>
2022-06-14 21:21:54 +0200

    Added CimtCraft.java

---
+++ src/main/java/club/psychose/cimtcraft/CimtCraft.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright © 2022 psychose.club
+ * Discord: https://www.psychose.club/discord
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package club.psychose.cimtcraft;
+
+import club.psychose.cimtcraft.utils.Constants;
+import club.psychose.cimtcraft.utils.logging.ConsoleLogger;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * <p>This class is the main class of this plugin, it would be called when the server loads the plugins.</p>
+ * @author CrashedLife
+ */
+
+public final class CimtCraft extends JavaPlugin {
+    @Override
+    public void onEnable () {
+        System.out.println("""
+                  ___  __  _  _  ____  ___  ____   __   ____  ____\s
+                 / __)(  )( \\/ )(_  _)/ __)(  _ \\ / _\\ (  __)(_  _)
+                ( (__  )( / \\/ \\  )( ( (__  )   //    \\ ) _)   )( \s
+                 \\___)(__)\\_)(_/ (__) \\___)(__\\_)\\_/\\_/(__)   (__)\s
+                """);
+        ConsoleLogger.debug("Copyright © 2022 psychose.club");
+        ConsoleLogger.debug("Version: " + Constants.VERSION);
+        ConsoleLogger.debug("Build Version: " + Constants.BUILD);
+        ConsoleLogger.printEmptyLine();
+        ConsoleLogger.debug("Initialize plugin features...");
+        Constants.FEATURE_MANAGER.initializeFeatures();
+        ConsoleLogger.debug("Plugin features initialized!");
+    }
+
+    @Override
+    public void onDisable () {
+        ConsoleLogger.debug("CimtCraft disabled!");
+    }
+}
