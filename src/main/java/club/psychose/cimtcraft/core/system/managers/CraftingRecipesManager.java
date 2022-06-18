832da51761009bae037ba8a4a313c9a1b8319107
CrashedLife <33805688+CrashedLife@users.noreply.github.com>
2022-06-18 10:55:11 +0200

    Added new recipes in CraftingRecipesManager.java

--- src/main/java/club/psychose/cimtcraft/core/system/managers/CraftingRecipesManager.java
+++ src/main/java/club/psychose/cimtcraft/core/system/managers/CraftingRecipesManager.java
@@ -114,6 +114,18 @@ public final class CraftingRecipesManager {
             this.craftingRecipesArrayList.add(new ZombieVillagerSpawnEggRecipe().getCraftingRecipe());
             this.craftingRecipesArrayList.add(new ZombifiedPiglinSpawnEggRecipe().getCraftingRecipe());
         }
+
+        // Saddle.
+        if (Constants.FEATURE_MANAGER.isFeatureEnabled("Craft Saddle")) {
+            this.craftingRecipesArrayList.add(new SaddleRecipe().getCraftingRecipe());
+        }
+
+        // Horse Armor.
+        if (Constants.FEATURE_MANAGER.isFeatureEnabled("Craft Horse Armor")) {
+            this.craftingRecipesArrayList.add(new IronHorseArmorRecipe().getCraftingRecipe());
+            this.craftingRecipesArrayList.add(new GoldHorseArmorRecipe().getCraftingRecipe());
+            this.craftingRecipesArrayList.add(new DiamondHorseArmorRecipe().getCraftingRecipe());
+        }
     }
 
     /**
